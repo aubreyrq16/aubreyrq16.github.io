77b046f327b76f9b431cb5c350ea17f4789cf62b
Aubrey Quisumbing <65883592+aubreyrq16@users.noreply.github.com>
2025-09-18 01:14:05 +0800

    revamp ui initial

--- js/main.js
+++ js/main.js
@@ -1,21 +1,6 @@
-/*global $, jQuery, alert*/
-$(document).ready(function () {
-  'use strict'
-
-  var portfolioIsotope = $('.portfolio-container').isotope({
-    itemSelector: '.portfolio-thumbnail',
-    layoutMode: 'fitRows'
-  })
-
-  $('#filter-portfolio-nav .filter').on('click', function () {
-    $('#filter-portfolio-nav .filter').removeClass('font-bold')
-    $(this).addClass('font-bold')
-
-    portfolioIsotope.isotope({ filter: $(this).data('filter') })
-  })
-});
-
 // Initialize Lenis
 const lenis = new Lenis({
   autoRaf: true,
 });
+
+AOS.init();
